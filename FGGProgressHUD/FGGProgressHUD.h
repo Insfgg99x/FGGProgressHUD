d216965b4ae0a56b7a16a116fcd800f32cf12e0d
xgf <newbox0512@yahoo.com>
2015-08-19 13:19:24 +0800

    重构代码

--- FGGProgressHUD/FGGProgressHUD.h
+++ FGGProgressHUD/FGGProgressHUD.h
@@ -17,7 +17,7 @@
 #import <UIKit/UIKit.h>
 
 #define kDimsViewTag 5355321
-#define RGB(r,g,b) ([UIColor colorWithRed:(r)/255.0f green:(g)/255.0f blue:(b)/255.0f alpha:1.0f])
+#define RGB(r,g,b,a) ([UIColor colorWithRed:(r)/255.0f green:(g)/255.0f blue:(b)/255.0f alpha:(a)])
 /**
  *  加载提示标签
  */
--- FGGProgressHUD/FGGProgressHUD.m
+++ FGGProgressHUD/FGGProgressHUD.m
@@ -17,7 +17,9 @@
     CGFloat xpos=v.frame.size.width/2-80;
     CGFloat ypos=v.frame.size.height/2;
     FGGProgressHUD *loadingView=[[FGGProgressHUD alloc]initWithFrame:CGRectMake(xpos, ypos, 160, 60)];
-    loadingView.backgroundColor=[UIColor clearColor];
+    loadingView.backgroundColor=RGB(71, 71, 71,0.3);
+    loadingView.layer.cornerRadius=6;
+    loadingView.clipsToBounds=YES;
     loadingView.text=@"    Loading...";
     loadingView.textColor=[UIColor whiteColor];
     loadingView.textAlignment=NSTextAlignmentCenter;
@@ -28,14 +30,7 @@
     [indicator startAnimating];
     indicator.frame=CGRectMake(10, 15, 30, 30);
     [loadingView addSubview:indicator];
-    //半透明遮罩
-    UIView *dimsView=[[UIView alloc] initWithFrame:loadingView.frame];
-    dimsView.backgroundColor=RGB(71, 71, 71);
-    dimsView.alpha=0.4;
-    dimsView.layer.cornerRadius=5;
-//    dimsView.clipsToBounds=YES;
-    dimsView.tag=kDimsViewTag;
-    [v addSubview:dimsView];
+
     [UIView animateWithDuration:0.3 animations:^{
         [v addSubview:loadingView];
     }];
@@ -52,8 +47,6 @@
                 [sub removeFromSuperview];
             }];
         }
-        if(sub.tag==kDimsViewTag)
-            [sub removeFromSuperview];
     }
 }
 @end
